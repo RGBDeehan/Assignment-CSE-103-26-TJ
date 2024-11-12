d246bba9f733df81935bd2d6ce0b0ec6be2fc80b
RGBDeehan <rageebyasardeehan@gmail.com>
2024-11-12 21:09:15 +0600

    Update LW5.c

--- LW5.c
+++ LW5.c
@@ -11,28 +11,28 @@ float x,y,z,d;
     printf("Enter the 3rd number: ");
     scanf("%f",&z);
 
-   if (x>y && x>z){
-   if(y<x && y<z){
+   if (x>=y && x>=z){
+   if(y<=x && y<=z){
     d = x -y;
    }
-   else if(z<x && z<y){
+   else if(z<=x && z<=y){
     d = x -z;
    }
    }
-if (z>y && z>x){
-   if(y<x && y<z){
+if (z>=y && z>=x){
+   if(y<=x && y<=z){
     d = z -y;
    }
-   else if(x<z && x<y){
+   else if(x<=z && x<=y){
     d = z-x;
    }
    }
 
-   if (y>x && y>z){
-   if(x<y && x<z){
+   if (y>=x && y>=z){
+   if(x<=y && x<=z){
     d = y-x;
    }
-   else if(z<x && z<y){
+   else if(z<=x && z<=y){
     d = y -z;
    }
    }
