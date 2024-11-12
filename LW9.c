993addf2a061a717f81431bb0dfec4b14d1a5661
RGBDeehan <rageebyasardeehan@gmail.com>
2024-11-12 19:14:29 +0600

    Update LW9.c

--- LW9.c
+++ LW9.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main(){
 float x,y,z ;
-    printf("Enter the 1st hand of trianfle:");
+    printf("Enter the 1st hand of triangle:");
     scanf("%f",&x);
 
-    printf("Enter the 2nd hand of trianfle:");
+    printf("Enter the 2nd hand of triangle:");
     scanf("%f",&y);
 
-    printf("Enter the 3rd hand of trianfle:");
+    printf("Enter the 3rd hand of triangle:");
     scanf("%f",&z);
 
 if ( x+y>z && y+z>x && z+x>y ){
