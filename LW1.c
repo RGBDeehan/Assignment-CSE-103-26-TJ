b1cf3f67137e1da35324a1220dff6a4e6feb6b76
RGBDeehan <rageebyasardeehan@gmail.com>
2024-11-15 19:21:34 +0600

    Update LW1.c

--- LW1.c
+++ LW1.c
@@ -4,7 +4,7 @@ int x;
     printf("Enter the number: ");
     scanf("%d",&x);
 
-    if ( x % 5 == 0) {
+    if ((int) x % 5 == 0) {
         printf("The number is divisible by 5");
     }
 else {printf("The number is not divisible by 5");
