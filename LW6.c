8f7f997a86edc3337d38311898cb2e99a0767954
RGBDeehan <rageebyasardeehan@gmail.com>
2024-11-19 11:12:59 +0600

    Update LW6.c

--- LW6.c
+++ LW6.c
@@ -5,13 +5,13 @@ float x , y;
     scanf("%f",&x);
 
     if ( x <= 10000) {
-       y = x+(x *0.2 *0.8);
+       y = x+(x *0.2 )+(x*0.8);
     }
 else if( x > 10000 && x <= 20000) {
-    y = x + ( x * 0.25 * 0.9 );
+    y = x + ( x * 0.25 )+(x* 0.9 );
 }
  else if ( x > 20000) {
-    y = x + ( x * 0.3 * 0.95 );
+    y = x + ( x * 0.3 )+(x* 0.95 );
 }
 
 printf("The gross salary is: %.2f",y);
