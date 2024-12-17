c9f22720004967c13f55ba788f7296cc9e5065fd
RGBDeehan <rageebyasardeehan@gmail.com>
2024-12-17 16:41:35 +0600

    Update LW6.c

--- LW6.c
+++ LW6.c
@@ -1,3 +1,5 @@
+/* Write a C program to input basic salary of an employee and calculate its Gross salary according to following: Basic Salary <= 10000 : HRA = 20%, DA = 80% Basic Salary <= 20000 : HRA = 25%, DA = 90% Basic Salary > 20000 : HRA = 30%, DA = 95% */
+
 #include <stdio.h>
 int main(){
 float x , y;
